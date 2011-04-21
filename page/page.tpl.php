a64d9875a91cbc9b22feccfab4df8f36c1863555
Tirdad <tirdadc@383630.no-reply.drupal.org>
2011-04-21 16:37:11 -0400

    Render  if it's set

--- page/page.tpl.php
+++ page/page.tpl.php
@@ -114,7 +114,7 @@
 
       <?php if ($action_links): ?>
         <ul class="action-links">
-          <?php //print render($action_links); ?>
+          <?php print render($action_links); ?>
         </ul>
       <?php endif; ?>
 
